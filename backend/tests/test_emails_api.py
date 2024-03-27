462f5521e3c68b3e0da8e9c17caf71477fc748f5
Valwolfor <diegofelipe.gallo@gmail.com>
2024-03-27 09:58:40 -0700

    mod fly.tom

--- backend/tests/test_emails_api.py
+++ backend/tests/test_emails_api.py
@@ -16,7 +16,7 @@ def client(app):
 
 def test_emails_inbox(client):
     # TODO: This shouldn't be hard coded but accessing with function gives: OutOfContextError
-    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1MzY3NywianRpIjoiY2M3YmRkMjgtMWRiYS00NWI4LWI1NjctNzI5ZGYwMjJhNDM2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTM2NzcsImNzcmYiOiIwMmZiMWFkYi0yMDNhLTRmZjMtYmM1Ni01ZGMzMGViZWNiZGUifQ.Wnp2j0TU2OnUSFMMQD6V5JWX7K1U6SCDXygNrGQGMSg"  # create_access_token("carlosquintero2@gmail.com", expires_delta=False)
+    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1NTE5MCwianRpIjoiZGRiOTQzMDYtMjg3Yi00MGJlLThjNjYtNTdiMjdiZDVlY2FlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTUxOTAsImNzcmYiOiI3MGY0NWQ4My1mMTkxLTRkN2YtOGM5MS0xYTUxYTc4YWNiOGUifQ.sQ71faNI47vvA3jJmp8qszVgtVQgRstiJuIUwW_badk"  # create_access_token("carlosquintero2@gmail.com", expires_delta=False)
     headers = {"Authorization": "Bearer {}".format(access_token)}
 
     response = client.get("/api/emails/inbox", headers=headers)
@@ -33,7 +33,7 @@ def test_emails_inbox(client):
 def test_emails_sent(client):
 
     # TODO: This shouldn't be hard coded but accessing with function gives: OutOfContextError
-    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1MzY3NywianRpIjoiY2M3YmRkMjgtMWRiYS00NWI4LWI1NjctNzI5ZGYwMjJhNDM2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTM2NzcsImNzcmYiOiIwMmZiMWFkYi0yMDNhLTRmZjMtYmM1Ni01ZGMzMGViZWNiZGUifQ.Wnp2j0TU2OnUSFMMQD6V5JWX7K1U6SCDXygNrGQGMSg"  # create_access_token("carlosquintero2@gmail.com", expires_delta=False)
+    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1NTE5MCwianRpIjoiZGRiOTQzMDYtMjg3Yi00MGJlLThjNjYtNTdiMjdiZDVlY2FlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTUxOTAsImNzcmYiOiI3MGY0NWQ4My1mMTkxLTRkN2YtOGM5MS0xYTUxYTc4YWNiOGUifQ.sQ71faNI47vvA3jJmp8qszVgtVQgRstiJuIUwW_badk"  # create_access_token("carlosquintero2@gmail.com", expires_delta=False)
     headers = {"Authorization": "Bearer {}".format(access_token)}
 
     response = client.get("/api/emails/sent", headers=headers)
@@ -48,10 +48,10 @@ def test_emails_sent(client):
 
 
 def test_one_get_email_inbox(client):
-    primary_key = "f1af4546-989e-4b01-a5ea-baf90ca4dbb0"
+    primary_key = "b3b081d4-2a6f-49c3-91f8-cc220ea04617"
 
     # TODO: This shouldn't be hard coded but accessing with function gives: OutOfContextError
-    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1MzY3NywianRpIjoiY2M3YmRkMjgtMWRiYS00NWI4LWI1NjctNzI5ZGYwMjJhNDM2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTM2NzcsImNzcmYiOiIwMmZiMWFkYi0yMDNhLTRmZjMtYmM1Ni01ZGMzMGViZWNiZGUifQ.Wnp2j0TU2OnUSFMMQD6V5JWX7K1U6SCDXygNrGQGMSg"  # create_access_token("carlosquintero2@gmail.com", expires_delta=False)
+    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1NTE5MCwianRpIjoiZGRiOTQzMDYtMjg3Yi00MGJlLThjNjYtNTdiMjdiZDVlY2FlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTUxOTAsImNzcmYiOiI3MGY0NWQ4My1mMTkxLTRkN2YtOGM5MS0xYTUxYTc4YWNiOGUifQ.sQ71faNI47vvA3jJmp8qszVgtVQgRstiJuIUwW_badk"  # create_access_token("carlosquintero2@gmail.com", expires_delta=False)
     headers = {"Authorization": "Bearer {}".format(access_token)}
 
     response = client.get("/api/emails/inbox/{}".format(primary_key), headers=headers)
@@ -66,11 +66,11 @@ def test_one_get_email_inbox(client):
 
 
 def test_one_get_email_sent(client):
-    primary_key = "f1af4546-989e-4b01-a5ea-baf90ca4dbb0"
+    primary_key = "b3b081d4-2a6f-49c3-91f8-cc220ea04617"
 
     # TODO: This shouldn't be hard coded but accessing with function gives: OutOfContextError
 
-    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1MzY3NywianRpIjoiY2M3YmRkMjgtMWRiYS00NWI4LWI1NjctNzI5ZGYwMjJhNDM2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTM2NzcsImNzcmYiOiIwMmZiMWFkYi0yMDNhLTRmZjMtYmM1Ni01ZGMzMGViZWNiZGUifQ.Wnp2j0TU2OnUSFMMQD6V5JWX7K1U6SCDXygNrGQGMSg" #create_access_token("carlosquintero2@gmail.com", expires_delta=False)
+    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU1NTE5MCwianRpIjoiZGRiOTQzMDYtMjg3Yi00MGJlLThjNjYtNTdiMjdiZDVlY2FlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImNhcmxvc3F1aW50ZXJvMkBnbWFpbC5jb20iLCJuYmYiOjE3MTE1NTUxOTAsImNzcmYiOiI3MGY0NWQ4My1mMTkxLTRkN2YtOGM5MS0xYTUxYTc4YWNiOGUifQ.sQ71faNI47vvA3jJmp8qszVgtVQgRstiJuIUwW_badk" #create_access_token("carlosquintero2@gmail.com", expires_delta=False)
     headers = {"Authorization": "Bearer {}".format(access_token)}
 
     response = client.get("/api/emails/sent/{}".format(primary_key), headers=headers)
